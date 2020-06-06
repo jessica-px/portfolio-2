3498a081f1202ea64ad88e77a20adb1419909f5d
Jessica Peck <jessypeck@gmail.com>
2020-06-05 19:59:46 -0700

    Import styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import styled from "styled-components"
 
 import Gallery from '../components/Gallery'
 import Layout from '../components/layout'
